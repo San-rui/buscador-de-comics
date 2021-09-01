b0c2a9ba1c96f14e4e7903902896b256d0b18586
San <sruidiaz@hotmail.com>
2021-09-01 18:08:59 +0200

    search panel and styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,126 @@
+//-----------TYPE---------------
+// type Control ={
+// 	type: string,
+// 	name: string,
+// 	id: string,
+// 	options: option[]
+// }
+//-------------Header-------------
 var createHeader = function () {
     var header = document.createElement('header');
     var mainTitle = document.createElement('h1');
-    var containerImg = document.createElement('div');
+    var headerImg = document.createElement('img');
     mainTitle.innerHTML = "ADA COMICS";
-    //headerImg.setAttribute('src','./assets/images/pngwing.com.png');
+    headerImg.setAttribute('src', './assets/images/pngwing.com.png');
     header.classList.add('primary-header');
     header.appendChild(mainTitle);
-    header.appendChild(containerImg);
+    header.appendChild(headerImg);
     document.body.appendChild(header);
 };
 createHeader();
+//-------------SEARCH BAR-------------
+var controlsSearch = [
+    {
+        type: "text",
+        name: "addSearch",
+        id: "addSearch"
+    },
+    {
+        type: "select",
+        name: "Tipo",
+        id: "type",
+        options: [
+            {
+                id: "comic",
+                name: "Comic"
+            },
+            {
+                id: "personajes",
+                name: "Personajes"
+            },
+        ]
+    },
+    {
+        type: "select",
+        name: "Orden",
+        id: "order",
+        options: [
+            {
+                id: "a-z",
+                name: "A-Z"
+            },
+            {
+                id: "z-a",
+                name: "Z-A"
+            },
+            {
+                id: "mas-nuevos",
+                name: "Más nuevos"
+            },
+            {
+                id: "mas-viejos",
+                name: "Más viejos"
+            },
+        ]
+    },
+];
+var container = document.createElement('div');
+var main = document.createElement('main');
+container.classList.add('container');
+container.appendChild(main);
+var formSearch = document.createElement("form");
+formSearch.classList.add('form-search');
+var searchContainerGeneral = document.createElement('div');
+var placeHolderText = "Ingresa tu búsqueda";
+var icon = document.createElement('i');
+icon.classList.add("fas", "fa-search");
+//searchContainerGeneral.classList.add('search-container')
+var makeForm = function (form, ctrls, parent, containerSearch) {
+    var tittleH2 = document.createElement('h2');
+    tittleH2.appendChild(document.createTextNode("Búsqueda"));
+    var inputContainer = document.createElement('div');
+    inputContainer.classList.add('input-container');
+    var selectContainer = document.createElement('div');
+    selectContainer.classList.add('select-container');
+    for (var _i = 0, ctrls_1 = ctrls; _i < ctrls_1.length; _i++) {
+        var control = ctrls_1[_i];
+        var elem = void 0;
+        if (control.type === "select") {
+            var labelselect = document.createElement('label');
+            labelselect.appendChild(document.createTextNode(control.name));
+            selectContainer.appendChild(labelselect);
+        }
+        switch (control.type) {
+            case "select":
+                elem = document.createElement("select");
+                for (var _a = 0, _b = control.options; _a < _b.length; _a++) {
+                    var option = _b[_a];
+                    var op = document.createElement("option");
+                    elem.appendChild(op);
+                    op.value = option.id.toString();
+                    op.appendChild(document.createTextNode(option.name));
+                    selectContainer.appendChild(elem);
+                }
+                break;
+            default:
+                elem = document.createElement("input");
+                elem.type = control.type;
+                elem.placeholder = placeHolderText;
+                inputContainer.appendChild(icon);
+                inputContainer.appendChild(elem);
+                console.log(elem);
+                break;
+        }
+    }
+    var button = document.createElement("button");
+    button.type = "submit";
+    button.appendChild(document.createTextNode("Buscar"));
+    selectContainer.appendChild(button);
+    form.appendChild(inputContainer);
+    form.appendChild(selectContainer);
+    containerSearch.appendChild(tittleH2);
+    containerSearch.appendChild(form);
+    parent.appendChild(containerSearch);
+    document.body.appendChild(parent);
+};
+makeForm(formSearch, controlsSearch, container, searchContainerGeneral);
--- assets/ts/script.ts
+++ assets/ts/script.ts
@@ -1,16 +1,146 @@
+//-----------TYPE---------------
+// type Control ={
+// 	type: string,
+// 	name: string,
+// 	id: string,
+// 	options: option[]
+// }
+
+
+//-------------Header-------------
+
 const createHeader = ()=>{
     const header = document.createElement('header');
     const mainTitle = document.createElement('h1');
-    const containerImg = document.createElement('div');
+    const headerImg = document.createElement('img');
 
     mainTitle.innerHTML="ADA COMICS"
-    //headerImg.setAttribute('src','./assets/images/pngwing.com.png');
+    headerImg.setAttribute('src','./assets/images/pngwing.com.png');
     header.classList.add('primary-header');
 
     header.appendChild(mainTitle);
-    header.appendChild(containerImg);
+    header.appendChild(headerImg);
     document.body.appendChild(header);
-
 }
+createHeader();
 
-createHeader()
+//-------------SEARCH BAR-------------
+const controlsSearch = [
+	{
+		type: "text",
+		name: "addSearch",
+		id: "addSearch",
+	},
+	{
+		type: "select",
+		name: "Tipo",
+		id: "type",
+		options: [
+			{
+				id: "comic",
+				name: "Comic",
+			},
+			{
+				id: "personajes",
+				name: "Personajes",
+			},
+		],
+	},
+	{
+		type: "select",
+		name: "Orden",
+		id: "order",
+		options: [
+			{
+				id: "a-z",
+				name: "A-Z",
+			},
+			{
+				id: "z-a",
+				name: "Z-A",
+			},
+            {
+				id: "mas-nuevos",
+				name: "Más nuevos",
+			},
+			{
+				id: "mas-viejos",
+				name: "Más viejos",
+			},
+		],
+	},
+];
+
+
+
+const container= document.createElement('div');
+const main= document.createElement('main');
+container.classList.add('container')
+
+container.appendChild(main);
+const formSearch = document.createElement("form");
+formSearch.classList.add('form-search')
+const searchContainerGeneral = document.createElement('div');
+const placeHolderText="Ingresa tu búsqueda";
+const icon = document.createElement('i');
+icon.classList.add("fas", "fa-search");
+//searchContainerGeneral.classList.add('search-container')
+
+const makeForm = (form, ctrls, parent, containerSearch) => {
+
+    const tittleH2= document.createElement('h2');
+    tittleH2.appendChild(document.createTextNode("Búsqueda"));
+    const inputContainer = document.createElement('div');
+	inputContainer.classList.add('input-container')
+    const selectContainer = document.createElement('div');
+    selectContainer.classList.add('select-container')
+	
+    for (const control of ctrls) {
+		let elem;
+
+        if(control.type === "select"){
+            const labelselect = document.createElement('label');
+            labelselect.appendChild(document.createTextNode(control.name));
+            selectContainer.appendChild(labelselect);
+        }
+
+		switch (control.type) {
+			
+			case "select":
+				elem = document.createElement("select");
+				for (const option of control.options) {
+					const op = document.createElement("option");
+					elem.appendChild(op);
+					op.value = option.id.toString();
+					op.appendChild(document.createTextNode(option.name));
+                    
+                    selectContainer.appendChild(elem);
+				}
+				break;
+
+			default:
+				elem = document.createElement("input");
+				elem.type = control.type;
+                elem.placeholder=placeHolderText;
+                inputContainer.appendChild(icon)
+                inputContainer.appendChild(elem);
+                console.log(elem)
+				break;
+		}
+
+	}
+
+	const button = document.createElement("button");
+	button.type = "submit";
+	button.appendChild(document.createTextNode("Buscar"));
+    selectContainer.appendChild(button);
+    form.appendChild(inputContainer);
+    form.appendChild(selectContainer);
+    containerSearch.appendChild(tittleH2);
+    containerSearch.appendChild(form); 
+	parent.appendChild(containerSearch);
+    document.body.appendChild(parent);
+
+};
+
+makeForm(formSearch, controlsSearch, container, searchContainerGeneral);
