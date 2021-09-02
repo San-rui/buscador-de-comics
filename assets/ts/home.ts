a56ba29ba12e2ef1934aee8253ca7850425ba805
San <sruidiaz@hotmail.com>
2021-09-02 20:49:31 +0200

    add img cards

--- assets/ts/home.ts
+++ assets/ts/home.ts
@@ -1,1 +1,25 @@
+const containerElement = document.createElement('div');
+containerElement.classList.add('img-style');
+main.appendChild(containerElement);
+let contentHTML = '';
 
+
+fetch(url)
+.then(res => res.json())
+.then((json)=>{
+    console.log(json.data, 'REST.JSON')
+    json.limit=40;
+    for(const hero of json.data.results){
+        let urlHero = hero.urls[0].url;
+        contentHTML += `
+            <div class="prueba">
+                <a href="${urlHero }">
+                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                </a>
+            </div>
+        ` 
+        containerElement.innerHTML = contentHTML;
+        main.appendChild(containerElement);
+    }
+    
+})
