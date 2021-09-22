b427a3c9b7085df3d34cae64a1ac55e7b2d8992a
San <sruidiaz@hotmail.com>
2021-09-22 15:48:04 +0200

    setting of info page with function to show information

--- assets/js/info.js
+++ assets/js/info.js
@@ -35,9 +35,74 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var _this = this;
-var storageSearch = JSON.parse(localStorage.getItem('to-search-storage'));
-var url3 = baseUrl1 + "?ts=1&apikey=" + apiKey + "&hash=" + hash + "&offset=0";
-var url4 = baseUrl2 + "?ts=1&apikey=" + apiKey + "&hash=" + hash + "&offset=0";
+var url3 = baseUrl + "?ts=1&apikey=" + apiKey + "&hash=" + hash + "&offset=0";
+var url4 = baseUrl + "?ts=1&apikey=" + apiKey + "&hash=" + hash + "&offset=0";
+var params3 = new URLSearchParams(window.location.search);
+var info = encodeURIComponent(params3.get('info'));
+var type = (params3.get("type")) ? (params3.get("type")) : "comics";
+var toSearchInfo = params3.get('wordToSearch');
+console.log(info);
+console.log(type);
+//------------------CREATE CARD INFO-------------------------
+var containerAllInfo = document.createElement('div');
+containerAllInfo.classList.add('container-all-info');
+var containerInfo = document.createElement('div');
+containerInfo.classList.add('container-info');
+var imgInfo = document.createElement('img');
+var title = document.createElement('h3');
+var titleDescription = document.createElement('h4');
+titleDescription.appendChild(document.createTextNode("Descripción:"));
+var description = document.createElement('p');
+var createComicCard = function (data) {
+    title.innerHTML = data[0].title;
+    var titlePublicationDate = document.createElement('h4');
+    titlePublicationDate.appendChild(document.createTextNode("Publicado:"));
+    var PublicationDate = document.createElement('p');
+    PublicationDate.innerHTML = data[0].modified;
+    var titleScreenwriter = document.createElement('h4');
+    titleScreenwriter.appendChild(document.createTextNode("Guionistas:"));
+    var screenwrite = document.createElement('p');
+    if (data[0].creators.items) {
+        console.log(data[0].creators.items);
+    }
+    //screenwrite.innerHTML=data[0].creators
+    description.innerHTML = data[0].description;
+    containerInfo.appendChild(title);
+    containerInfo.appendChild(titlePublicationDate);
+    containerInfo.appendChild(PublicationDate);
+    containerInfo.appendChild(titleScreenwriter);
+    containerInfo.appendChild(screenwrite);
+};
+var createInfoCard = function (data) {
+    var srcInfo = data[0].thumbnail.path + "." + data[0].thumbnail.extension;
+    imgInfo.setAttribute('src', srcInfo);
+    if (type == "comics") {
+        createComicCard(data);
+    }
+    else {
+        title.innerHTML = data[0].name;
+        description.innerHTML = data[0].description;
+        containerInfo.appendChild(title);
+    }
+    containerInfo.appendChild(titleDescription);
+    containerInfo.appendChild(description);
+    containerAllInfo.appendChild(imgInfo);
+    containerAllInfo.appendChild(containerInfo);
+    main.appendChild(containerAllInfo);
+};
+//------------------GET URL-------------------------
+var getURLInfo = function () {
+    var url = "";
+    if (type == "comics") {
+        url = "" + baseUrl + type + "/" + info + "?title=" + toSearchInfo + "&ts=1&apikey=" + apiKey + "&hash=" + hash + "&offset=0";
+    }
+    else if (type == "characters") {
+        url = "" + baseUrl + type + "/" + info + "?name=" + toSearchInfo + "&ts=1&apikey=" + apiKey + "&hash=" + hash + "&offset=0";
+    }
+    return url;
+};
+var urlToUseInfo = getURLInfo();
+//---------------GET INFO FROM API MARVEL-------------
 var getMarvelInfo = function (url) { return __awaiter(_this, void 0, void 0, function () {
     var response, items, listItems, resultsItems, _i, resultsItems_1, item;
     return __generator(this, function (_a) {
@@ -50,14 +115,16 @@ var getMarvelInfo = function (url) { return __awaiter(_this, void 0, void 0, fun
                 items = _a.sent();
                 listItems = items.data;
                 resultsItems = listItems.results;
+                //console.log("result",resultsItems[0].creators.items[1].name)
                 for (_i = 0, resultsItems_1 = resultsItems; _i < resultsItems_1.length; _i++) {
                     item = resultsItems_1[_i];
                     if (item.thumbnail.path == 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available') {
-                        item.thumbnail.path = './assets/images/clean';
+                        item.thumbnail.path = '../assets/images/clean';
                     }
                 }
+                createInfoCard(resultsItems);
                 return [2 /*return*/];
         }
     });
 }); };
-getMarvelInfo(url3);
+getMarvelInfo(urlToUseInfo);
--- assets/ts/info.ts
+++ assets/ts/info.ts
@@ -1,7 +1,94 @@
-const storageSearch = JSON.parse(localStorage.getItem('to-search-storage'));
 
-const url3: string = `${baseUrl1}?ts=1&apikey=${apiKey}&hash=${hash}&offset=0`;
-const url4: string = `${baseUrl2}?ts=1&apikey=${apiKey}&hash=${hash}&offset=0`;
+const url3: string = `${baseUrl}?ts=1&apikey=${apiKey}&hash=${hash}&offset=0`;
+const url4: string = `${baseUrl}?ts=1&apikey=${apiKey}&hash=${hash}&offset=0`;
+
+const params3 = new URLSearchParams(window.location.search)
+let info= encodeURIComponent(params3.get('info'));
+let type= (params3.get("type")) ? (params3.get("type")): "comics"
+let toSearchInfo = params3.get('wordToSearch');
+
+
+console.log(info)
+console.log(type)
+
+//------------------CREATE CARD INFO-------------------------
+const containerAllInfo= document.createElement('div');
+containerAllInfo.classList.add('container-all-info');
+
+const containerInfo = document.createElement('div');
+containerInfo.classList.add('container-info');
+
+const imgInfo = document.createElement('img');
+const title= document.createElement('h3');
+const titleDescription= document.createElement('h4');
+titleDescription.appendChild(document.createTextNode("Descripción:"));
+const description= document.createElement('p');
+
+const createComicCard = (data)=>{
+    title.innerHTML=data[0].title;
+
+    const titlePublicationDate= document.createElement('h4');
+    titlePublicationDate.appendChild(document.createTextNode("Publicado:"));
+    const PublicationDate= document.createElement('p');
+    PublicationDate.innerHTML=data[0].modified;
+
+    const titleScreenwriter= document.createElement('h4');
+    titleScreenwriter.appendChild(document.createTextNode("Guionistas:"));
+    const screenwrite= document.createElement('p');
+
+    if(data[0].creators.items){
+        console.log(data[0].creators.items)
+    }
+    
+    //screenwrite.innerHTML=data[0].creators
+
+
+    description.innerHTML=data[0].description;
+
+    containerInfo.appendChild(title);
+    
+    containerInfo.appendChild(titlePublicationDate);
+    containerInfo.appendChild(PublicationDate);
+
+    containerInfo.appendChild(titleScreenwriter);
+    containerInfo.appendChild(screenwrite);
+}
+
+const createInfoCard = (data)=>{
+    
+    const srcInfo=`${data[0].thumbnail.path}.${data[0].thumbnail.extension}`
+    imgInfo.setAttribute('src', srcInfo);
+    if(type=="comics"){
+        createComicCard(data);
+    }else{
+        title.innerHTML=data[0].name;
+        description.innerHTML=data[0].description;
+        containerInfo.appendChild(title);
+    }
+    
+    containerInfo.appendChild(titleDescription);
+    containerInfo.appendChild(description);
+
+    containerAllInfo.appendChild(imgInfo);
+    containerAllInfo.appendChild(containerInfo);
+    main.appendChild(containerAllInfo);
+};
+
+//------------------GET URL-------------------------
+
+const getURLInfo = () =>{
+    let url="";
+    if(type=="comics"){
+        url=  `${baseUrl}${type}/${info}?title=${toSearchInfo}&ts=1&apikey=${apiKey}&hash=${hash}&offset=0`;
+    } else if(type=="characters"){
+        url=  `${baseUrl}${type}/${info}?name=${toSearchInfo}&ts=1&apikey=${apiKey}&hash=${hash}&offset=0`;
+    }
+    return url;
+}
+
+const urlToUseInfo= getURLInfo();
+
+//---------------GET INFO FROM API MARVEL-------------
 
 const getMarvelInfo = async(url)=>{
     const response = await fetch(url);
@@ -10,13 +97,17 @@ const getMarvelInfo = async(url)=>{
     const listItems= items.data;
     const resultsItems= listItems.results;
 
+    //console.log("result",resultsItems[0].creators.items[1].name)
+
     for(const item of resultsItems){
         if(item.thumbnail.path== 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available'){
-            item.thumbnail.path='./assets/images/clean'
+            item.thumbnail.path='../assets/images/clean'
         }
     }
 
+    createInfoCard(resultsItems)
+
 }
 
-getMarvelInfo(url3);
+getMarvelInfo(urlToUseInfo);
 
